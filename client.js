9513e862db7a473f52b3d38cf47bc60a12be88be
Edu <imsofrqsth@gmail.com>
2025-05-07 16:10:13 +0200

    First Commit

---
+++ client.js
@@ -0,0 +1,79 @@
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const socket = new WebSocket('ws://' + location.hostname + ':3000');
+
+let players = {};
+let bullets = [];
+let myId = null;
+
+const keys = {};
+let mouse = { x: 0, y: 0 };
+
+document.addEventListener('keydown', e => keys[e.key] = true);
+document.addEventListener('keyup', e => keys[e.key] = false);
+canvas.addEventListener('mousedown', e => {
+    const angle = Math.atan2(e.offsetY - players[myId].y, e.offsetX - players[myId].x);
+    socket.send(JSON.stringify({ type: 'shoot', angle }));
+});
+canvas.addEventListener('mousemove', e => {
+    mouse.x = e.offsetX;
+    mouse.y = e.offsetY;
+});
+
+function drawPlayer(player, isSelf = false) {
+    ctx.fillStyle = isSelf ? 'lime' : 'red';
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.fillStyle = 'white';
+    ctx.fillText(player.hp + " HP", player.x - 15, player.y - 15);
+}
+
+function drawBullet(bullet) {
+    ctx.fillStyle = 'yellow';
+    ctx.beginPath();
+    ctx.arc(bullet.x, bullet.y, 3, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (myId && players[myId]) {
+        let me = players[myId];
+        if (keys['w']) me.y -= 2;
+        if (keys['s']) me.y += 2;
+        if (keys['a']) me.x -= 2;
+        if (keys['d']) me.x += 2;
+        socket.send(JSON.stringify({ type: 'move', x: me.x, y: me.y }));
+    }
+
+    for (let id in players) {
+        drawPlayer(players[id], id === myId);
+    }
+
+    for (let bullet of bullets) {
+        drawBullet(bullet);
+    }
+
+    requestAnimationFrame(gameLoop);
+}
+
+socket.onmessage = event => {
+    const msg = JSON.parse(event.data);
+
+    if (msg.type === 'init') {
+        myId = msg.id;
+        players = msg.players;
+    } else if (msg.type === 'update') {
+        players[msg.id] = msg.player;
+    } else if (msg.type === 'remove') {
+        delete players[msg.id];
+    } else if (msg.type === 'bullets') {
+        bullets = msg.bullets;
+    }
+};
+
+gameLoop();
---
+++ server.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const WebSocket = require('ws');
+
+// 1) HTTP server con Express
+app.use(express.static(path.join(__dirname))); // serve index.html, client.js, ecc.
+
+const server = http.createServer(app);
+
+// 2) WebSocket accorpato allo stesso server HTTP
+const wss = new WebSocket.Server({ server });
+
+let players = {};
+let bullets = [];
+let idCounter = 0;
+
+wss.on('connection', socket => {
+  const id = ++idCounter;
+  players[id] = { x:400, y:300, hp:100 };
+
+  // Invia init a questo client
+  socket.send(JSON.stringify({ type:'init', id, players }));
+
+  socket.on('message', msgStr => {
+    const msg = JSON.parse(msgStr);
+    // handle move / shoot come prima...
+    if (msg.type==='move' && players[id]) {
+      players[id].x = msg.x; players[id].y = msg.y;
+      broadcast({ type:'update', id, player:players[id] });
+    }
+    else if (msg.type==='shoot' && players[id]) {
+      const speed = 5;
+      bullets.push({
+        x: players[id].x,
+        y: players[id].y,
+        dx: Math.cos(msg.angle)*speed,
+        dy: Math.sin(msg.angle)*speed,
+        owner: id,
+        life: 60
+      });
+    }
+  });
+
+  socket.on('close', () => {
+    delete players[id];
+    broadcast({ type:'remove', id });
+  });
+});
+
+function broadcast(o) {
+  const s = JSON.stringify(o);
+  wss.clients.forEach(c => {
+    if (c.readyState === WebSocket.OPEN) c.send(s);
+  });
+}
+
+// aggiornamento proiettili + collisioni (come prima)...
+function updateBullets() {
+  for (let b of bullets) {
+    b.x += b.dx; b.y += b.dy; b.life--;
+    // collision detection...
+    for (let pid in players) {
+      if (pid!=b.owner && players[pid]) {
+        const p = players[pid];
+        if (Math.hypot(p.x-b.x, p.y-b.y) < 12) {
+          p.hp -= 20;
+          if (p.hp <= 0) delete players[pid], broadcast({ type:'remove', id:pid });
+          else broadcast({ type:'update', id:pid, player:p });
+          b.life = 0;
+        }
+      }
+    }
+  }
+  bullets = bullets.filter(b => b.life>0);
+  broadcast({ type:'bullets', bullets });
+}
+
+setInterval(updateBullets, 1000/30);
+
+// 3) Avvia tutto su porta 3000
+server.listen(3000, () => console.log('Game running at http://localhost:3000'));
